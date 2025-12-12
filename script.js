765f377a8285531f6312088b0cc05046b3f03604
KashanAdnan02 <fullstackdevkashan@gmail.com>
2025-12-12 17:43:06 +0500

    second class learning if statment revised variable

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("Hello, World!")
+
+// Variable Types
+// Var // Global Variable
+// block
+// {
+//     var age = 19
+// }
+// console.log(age);
+
+// Var Depreceated
+
+// const name2 = "Kashan"
+
+
+// let name2 = "Kashan"
+// name2 = "Adnan"
+
+
+
+// const name2 = "Kashan"
+// name2 = "Adnan"
+
+// console.log(name2);
+
+// Let Const
+
+
+// Let  // Blocked Scope Variable
+// Const // Blocked Scope Variable
+
+// variable name
+
+// assignment operator
+// = 
+
+
+// console.log(age)
+
+
+// let age = 29;
+/// if   agar(age === 90) // false
+
+
+
+
+// if(age === 90){ // true // false
+//     console.log("Hi, it is working!");   
+// }
+///
+
+// Number
+// String
