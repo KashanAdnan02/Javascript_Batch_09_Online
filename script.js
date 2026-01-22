db28a26effcc9e9f7c3e6e22ace42ee429a9de9a
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-01-22 20:29:52 +0500

    done

--- script.js
+++ script.js
@@ -121,7 +121,7 @@
 // console.log(array);
 
 // console.log(array.length);
- 
+
 
 //     0        1        2        3
 // ["Hello", "Iqra", "Noreen", "Kashan"]
@@ -155,8 +155,143 @@
 
 // exact same as forloop
 // while loop
-let i = 0
-while(i < 10){
-    console.log("it worked but in while loop")
-    i += 1
-}
+// let i = 0
+// while(i < 10){
+//     console.log("it worked but in while loop")
+//     i += 1
+// }
+
+
+
+
+
+
+// For Loop
+// Repeat 
+
+
+// console.log("Hello 1")
+// console.log("Hello 2")
+// console.log("Hello 3")
+// console.log("Hello 4");
+// console.log("Hello 5");
+
+
+// for
+// initialization condition increment
+//                0 < 11    true
+//                1 < 11    true
+//                2 < 11    true
+//                3 < 11    true
+//                4 < 11    true
+//                5 < 11    true
+//                6 < 11    true
+//                7 < 11    true
+//                8 < 11    true
+//                9 < 11    true
+//                10 < 11    true
+//    2 *             11 < 11    false
+//  2 X 1 = 2 
+//  2 X 2 = 4
+//  2 X 3 = 6
+//  2 X 4 = 8
+//  2 X 5 = 10 
+//  and so on
+// for(var i = 0;    i < 11;    i += 1){
+//     // code execute // Write
+//     console.log( * i)
+// }
+
+// 11 // 10
+// 0
+// 
+
+
+// var name = prompt("Enter your number")
+
+// console.log(name)
+
+
+
+
+
+
+
+
+
+// let array = ["Kashan", "Adnan", 14, 39483754235345, 3499]
+
+
+// let student = {
+//     firstname: "Kashan",
+//     lastname: "Adnan",
+//     age: 14,
+//     phone : 3472914253,
+//     roll_no : 3499
+// }
+
+
+
+// keyword
+// function
+
+
+
+
+
+
+// document object model
+
+// html 
+// document
+
+
+// console.log(document)
+
+
+// let heading1 = document.getElementById("heading1")
+
+
+// heading1.innerHTML = "Laptop"
+// heading1.style.color = "red"
+// heading1.style.backgroundColor = "yellow"
+
+
+
+
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//         console.log(xhttp.responseText)
+//     }
+// };
+// xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+// xhttp.send();
+
+// javascript own method/keyword/function .then .catch
+fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((data) => {
+        let data2 = data.json().then((data3) => {
+            console.log(data3);
+
+        }).catch((error2) => {
+            console.log(error2);
+
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+
+
+
+
+
+
+
+
+
+// 1) success (data backend se agaya) then
+// 2) fail (netword problem, backend stopped, database disconnected) catch
